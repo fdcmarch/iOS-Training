6973dc4b18b3b713bf659c17cee9f8a41132b56c
fdcmarch <fdc.crismarch@gmail.com>
2024-11-06 17:33:15 +0800

    day 3 changes

---
+++ iOSTraining/MorePhotos/PhotoCell/PhotoCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCell.swift
+//  iOSTraining
+//
+//  Created by FDC-CrisMarch on 11/6/24.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
